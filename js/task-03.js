898914f20c46109d25a91601849629e374c95d31
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-05-20 17:58:53 +0300

    Add files via upload

--- js/task-03.js
+++ js/task-03.js
@@ -15,5 +15,18 @@ const images = [
 
 const list = document.querySelector('.gallery');
 
-const element = images.map((image) => `<li><img src = ${imgage.url} alt = "${image.alt}"></li>`).join('');
-list.insertAdjacentHTML("beforeend", element)
+const element = images.map((image) => `<li><img src=${image.url} alt = ${image.alt}></li>`).join('');
+list.insertAdjacentHTML("beforeend", element);
+
+const items = list.querySelectorAll('li');
+items.forEach((item) => {
+  item.classList.add('gallery__item');
+});
+
+const styleImgs = list.querySelectorAll('img');
+styleImgs.forEach((img) => {
+  img.classList.add('gallery__image');
+});
+
+const head = document.querySelector('head');
+head.innerHTML += `<link rel="stylesheet" href="css/gallery-styles.css" />`;
