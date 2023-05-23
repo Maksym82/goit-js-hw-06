e9e00182eb6c021e98f446744893ec2b6ed6e7e5
Maksym <maksym.gromov82@gmail.com>
2023-05-23 15:16:04 +0300

    Создаёт задачу 6 и фиксит задачу 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,6 @@
 const textInput = document.querySelector('#name-input');
-const outPut = document.querySelector('#name-output');
+const textOutput = document.querySelector('#name-output');
 
 textInput.addEventListener("input", (event) => {
-    outPut.textContent = event.currentTarget.value;
+    textOutput.textContent = event.currentTarget.value;
 });
